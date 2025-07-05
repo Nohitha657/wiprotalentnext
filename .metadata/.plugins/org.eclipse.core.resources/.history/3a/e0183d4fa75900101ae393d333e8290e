233f25332dfddc0bcd7b2775119035d0df734b13
Nohitha657 <nohithachidaraboina@gmail.com>
2025-07-05 19:28:45 +0530

    added codes to previous classes

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/3a/e0183d4fa75900101ae393d333e8290e
@@ -0,0 +1,12 @@
+package Java_fundamentals;
+
+import java.util.Scanner;
+
+public class Question10 {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+	}
+
+}
--- Java_fundamentals/src/Java_fundamentals/Question7.java
+++ Java_fundamentals/src/Java_fundamentals/Question7.java
@@ -1,8 +1,18 @@
 package Java_fundamentals;
 
+import java.util.Scanner;
+
 public class Question7 {
 
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		char ch1=sc.next().toLowerCase().charAt(0);
+		char ch2=sc.next().toLowerCase().charAt(0);
+		if(ch1>ch2) {
+			System.out.print(ch2 +" , "+ch1);
+		}else {
+			System.out.print(ch1 +" , "+ch2);
+		}
 		// TODO Auto-generated method stub
 
 	}
